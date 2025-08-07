4c8afe28f79e651c1982fbf39ab335c6402ff86a
Luis Felipe Guerreiro Ota <filipeluis+github@gmail.com>
2025-08-07 14:54:01 -0300

    feat: project `hr-payroll` created

---
+++ hr-payroll/src/test/java/com/felipe/hrpayroll/HrPayrollApplicationTests.java
@@ -0,0 +1,13 @@
+package com.felipe.hrpayroll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrPayrollApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

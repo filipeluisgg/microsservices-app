4d81938e154e2a9d11daf44e6b548366c571a696
Luis Felipe Guerreiro Ota <filipeluis+github@gmail.com>
2025-08-09 11:01:21 -0300

    feat: implemented endpoint `POST /hr-oauth/oauth/token` to login with `JWT` token

--- hr-oauth/src/main/java/com/felipe/hroauth/services/UserService.java
+++ hr-oauth/src/main/java/com/felipe/hroauth/services/UserService.java
@@ -5,24 +5,29 @@ import com.felipe.hroauth.feignclients.UserFeignClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
-public class UserService
+public class UserService implements UserDetailsService
 {
-    private static Logger logger = LoggerFactory.getLogger(UserService.class);
-
+    private static final Logger logger = LoggerFactory.getLogger(UserService.class);
 
     @Autowired
     private UserFeignClient userFeignClient;
 
-    public User findByEmail(String email) {
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
         User user = userFeignClient.findByEmail(email).getBody();
         if (user == null) {
             logger.error("Email not found: " + email);
-            throw new IllegalArgumentException("Email not found");
+            throw new UsernameNotFoundException("Email not found");
         }
         logger.info("Email found: " + email);
         return user;
     }
 }
+

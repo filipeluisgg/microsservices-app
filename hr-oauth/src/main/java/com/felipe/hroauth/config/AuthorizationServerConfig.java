cd1122888bf89af48fb6e2213e2135574dca1406
Luis Felipe Guerreiro Ota <filipeluis+github@gmail.com>
2025-08-10 12:33:41 -0300

    refac: move hard-code configurations to a private gitHub config server

--- hr-oauth/src/main/java/com/felipe/hroauth/config/AuthorizationServerConfig.java
+++ hr-oauth/src/main/java/com/felipe/hroauth/config/AuthorizationServerConfig.java
@@ -1,6 +1,7 @@
 package com.felipe.hroauth.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -14,7 +15,14 @@ import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 @Configuration
 @EnableAuthorizationServer
-public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
+public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter
+{
+    @Value("${oauth.client.name}")
+    private String clientName;
+
+    @Value("${oauth.client.secret}")
+    private String clientSecret;
+
     @Autowired
     private BCryptPasswordEncoder bCryptPasswordEncoder;
 
@@ -36,8 +44,8 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
         clients.inMemory()
-                .withClient("name-app123")
-                .secret(bCryptPasswordEncoder.encode("secret-app123"))
+                .withClient(clientName)
+                .secret(bCryptPasswordEncoder.encode(clientSecret))
                 .scopes("read", "write")
                 .authorizedGrantTypes("password")
                 .accessTokenValiditySeconds(86400);
